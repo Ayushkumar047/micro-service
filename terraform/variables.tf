0aa5f39cb73278fcf2f5d63750b793236270c586
ayushkumar047 <pratapsingh.aayush047@gmail.com>
2021-08-22 16:11:15 +0530

    update terraform variables file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,19 @@
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
+variable vpc_cidr_block {
+	default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+	default = "10.0.10.0/24"
+}
+variable avail_zone {
+	default = "ap-south-1a"
+}
+variable env_prefix {
+	default = "dev"
+}
+variable my_ip {
+	default = "103.87.59.28/32"
+}
+variable instance_type {
+	default = "t2.micro"
+}
 
